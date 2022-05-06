0b354172c8c93d476e27e3151f755744959e0062
王巍 <13540025344@163.com>
2022-05-06 18:02:14 +0800

    增加分页

---
+++ commons/src/main/java/com/learaning/commons/config/MybatisPlusConfig.java
@@ -0,0 +1,22 @@
+package com.learaning.commons.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+
+@Configurable
+public class MybatisPlusConfig {
+
+    /**
+     * 新的分页插件
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
